f93f7180ffa9b6218073d67a5911d0f6c47deb7c
leyanelouiset <louisetleyane@gmail.com>
2024-12-13 12:29:41 +0100

    parti 1

---
+++ script.js
@@ -0,0 +1,33 @@
+//Vous devez écrire au moins 3 fonctions pour gérer :
+
+//Si la proposition est bien composée uniquement des 4 couleurs possibles et rien d’autre
+//Une fonction qui retourne true ou false si la bonne combinaison est trouvée ou non
+//Une fonction qui gère la partie (continuer tant que/fin si gagné)
+
+const color= ["violet","rose","bleu","jaune",]
+console.log(color)
+
+
+function displayColors() {
+    for (let i=0; i<color.length;i++) {
+        console.log(color[i])
+    }
+}
+
+displayColors()
+
+
+function booléan (){
+
+    if (color===color.length)
+    {console.log("true")}
+}
+
+booléan()
+
+
+
+//function play(){
+
+
+//}
